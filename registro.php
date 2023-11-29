83112c6c8fb435e5a9f07a85ea5f236c6e19bf98
Samuel Castro <dreift133@gmail.com>
2023-11-29 13:20:44 +0100

    subir archivos

---
+++ registro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="SASS/css/styles.css" rel="stylesheet"
+</head>
+
+<body>
+<header class="header">
+    <nav class="nav">
+        <a href="#" class="logo">
+            <img src="img/logobancosa.png" alt="" width="110px">
+        </a>
+        <a href="registro.php" class="button">Registro</a>
+        <a href="iniciarsesion.php" class="button">Iniciar sesrión</a>
+    </nav>
+</header>
+</body>
+</html>
