c3f8661d28933da693e7839224e1a02a58a52675
Samuel Castro <dreift133@gmail.com>
2023-11-30 10:18:44 +0100

    inicio de sesion creado

---
+++ inicarsesion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesiont</title>
+</head>
+<body>
+<div class="formulario">
+
+
+
+   
+
+<form method="post" action= "iniciarsesionconsultas.php">
+    <p>Iniciar sesion</p>
+    <div class="input-wrapper">
+        <input type="text" name="nombre" placeholder="Nombre">
+        <img class="input-icon" src="persona.png" alt=""width="30px">
+    </div>
+    <div class="input-wrapper">
+        <input type="password" name="contraseña" placeholder="Contraseña">
+        <img class="input-icon" src="candado.png" alt="" width="30px">
+
+    </div>
+    <div class="botones">
+    <input class="btn" type="submit" name="enviar" value="Iniciar sesion">
+    <p>¿Aun no tienes cuenta? <a href="registro.php">Regístrate</a></p
+   
+    </div>
+    
+
+</form>
+</div>
+</div>
+</body>
+</html>
