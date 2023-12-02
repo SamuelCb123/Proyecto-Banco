d1eb1ddef881ec75c2f6cee1d9ad9ed963b9b619
Samuel Castro <dreift133@gmail.com>
2023-12-02 15:01:09 +0100

    .

--- iniciarsesion.php
+++ iniciarsesion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesiont</title>
+    <title>Iniciar sesion</title>
     <link href="SASS/css/styles.css" rel="stylesheet"
 </head>
 <body>
