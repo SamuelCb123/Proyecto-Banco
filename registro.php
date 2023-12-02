44715bc86378b49d725aa78dc8de25176ce6ec8e
Samuel Castro <dreift133@gmail.com>
2023-12-02 23:05:28 +0100

    borrado

--- registro.php
+++ registro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="SASS/css/styles.css" rel="stylesheet">
+    <link href="css/css/style.css" rel="stylesheet">
 </head>
 
 <body>
