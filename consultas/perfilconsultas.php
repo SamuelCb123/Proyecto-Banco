d99ab7ae28049fec35766ee4ee7fbaecc066ece0
Samuel Castro <dreift133@gmail.com>
2023-12-03 21:42:37 +0100

    actualizacion consulta perfil mirar porque falla

--- consultas/perfilconsultas.php
+++ consultas/perfilconsultas.php
@@ -1,6 +1,5 @@
 <?php
 include('conexion.php');
-
 $nombre = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
 $codigopostal = $_POST['codigopostal'];
@@ -10,30 +9,8 @@ $ciudad = $_POST['ciudad'];
 $Pais = $_POST['Pais'];
 $provincia = $_POST['provincia'];
 
-$consulta = "UPDATE usuarios SET nombre=?, apellidos=?, codigopostal=?, fechadenacimiento=?, direccion=?, ciudad=?, Pais=?, provincia=? WHERE Pais=?";
-$stmt = mysqli_prepare($conexion, $consulta);
 
-// Check if the statement was prepared successfully
-if ($stmt) {
-    // Bind parameters
-    mysqli_stmt_bind_param($stmt, "sssssssss", $nombre, $apellidos, $codigopostal, $fechadenacimiento, $direccion, $ciudad, $Pais, $provincia, $Pais);
-
-    // Execute the statement
-    $resultado = mysqli_stmt_execute($stmt);
-
-    // Check for errors
-    if ($resultado) {
-        echo "Update successful";
-    } else {
-        echo "Error updating record: " . mysqli_error($conexion);
-    }
-
-    // Close the statement
-    mysqli_stmt_close($stmt);
-} else {
-    echo "Error preparing statement: " . mysqli_error($conexion);
-}
-
-// Close the connection
-mysqli_close($conexion);
+$consulta = "UPDATE usuarios SET nombre = '$nombre', apellidos= '$apellidos', codigopostal = '$codigopostal', fechadenacimiento ='$fechadenacimiento', direccion = '$direccion', ciudad = '$ciudad', Pais = '$Pais', provincia = '$provincia' WHERE Pais = :'$Pais' ";
+$resultado = mysqli_query($conexion, $consulta);
+ mysqli_close($conexion);
 ?>
