53c649d4bdc9c82d7cd84a06a7b2c26a8ff5a312
Samuel Castro <dreift133@gmail.com>
2023-12-04 20:03:15 +0100

    DNI VALIDADO

--- consultas/registroconsulta.php
+++ consultas/registroconsulta.php
@@ -1,11 +1,22 @@
 <?php
 include('conexion.php');
+function validarDNI($DNI){
+    return preg_match('/^[0-9]{8}[A-Za-z]$/', $DNI);
+}
 $nombre = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
 $DNI = $_POST['DNI'];
 $email = $_POST['email'];
 $Pais = $_POST['Pais'];
+$foto = $_FILES['foto'];
 
+$fotos = "Proyecto-Banco/img";
+
+
+if (!validarDNI($DNI)) {
+    echo "Error: El DNI no tiene el formato correcto.";
+    exit();
+}
 
 $consulta = "INSERT INTO usuarios (nombre, apellidos, DNI, email, Pais) VALUES ('$nombre', '$apellidos', '$DNI', '$email', '$Pais')";
 
